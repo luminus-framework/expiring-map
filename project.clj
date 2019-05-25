3c6f4b6dfcaecd02569401699a68f734cad9d235
dmitri.sotnikov@gmail.com <dmitri.sotnikov@gmail.com>
2019-05-25 11:33:53 -0400

    bumped up expiringmap

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-(defproject expiring-map "0.1.8"
+(defproject expiring-map "0.1.9"
   :description "a thread-safe map that expires entries"
   :url "https://github.com/yogthos/expiring-map"
   :license {:name "Apache 2.0 License"
             :url "https://www.apache.org/licenses/LICENSE-2.0.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [net.jodah/expiringmap "0.5.8"]]
+                 [net.jodah/expiringmap "0.5.9"]]
 
   :test-selectors {:default   (complement :benchmark)
                    :benchmark :benchmark
